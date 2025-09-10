05ff45f3d37b4efdca73d378eddf679cf1dd7074
kacoro <kacoro@qq.com>
2025-09-11 03:23:53 +0800

    改变部署方式

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
+  output: 'standalone', // 启用 Cloudflare 兼容性
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev']
   /* config options here */
 };
